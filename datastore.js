69b6744ca5f79153d700551c70078d4401c5c8a3
Federica Colapaoli <federica.colapaoli@coresecure.com>
2019-07-01 19:27:32 -0400

    first commitment

---
+++ datastore.js
@@ -0,0 +1,104 @@
+const STORE = [
+    {
+        question : "What is the name of the protagonists of the Anime 'Majokko Megu-chan'?",
+        answers: [
+            "Megu and Non",
+            "Bia and Noa",
+            "Catherine and Juliette",
+            "Sugar and Rune"
+    ], 
+        correctAnswer: "Megu and Non"
+    },
+    {
+        question : "In which era does the story of Manga 'Here comes Miss Modern' takes place?",
+        answers: [
+            "Meiji era",
+            "Edo era",
+            "Taisho era",
+            "Heian era"
+        ],
+        correctAnswer: "Taisho era"
+    },
+    {
+        question : "In 'Sailor Moon' who are the Soldiers of the Outer Solar System? ",
+        answers: [
+            "Jupiter, Neptune, Uranus",
+            "Pluto, Galaxia, Andromeda",
+            "Uranus, Venus, Mars", 
+            "Neptune, Pluto, Saturn"
+        ],
+        correctAnswer: "Neptune, Pluto, Saturn"
+    }, 
+    {
+        question : "In the Anime 'Creamy Mami, the Magic Angel' who grants the power to the protagonist, Yu? ",
+        answers: [
+            "Pino Pino",
+            "Palo Palo",
+            "Palo Palo",
+            "Puru Puru"
+        ],
+        correctAnswer : "Pino Pino"
+    },
+    {
+        question : "What’s the name of the Protagonist of the Anime 'Daitarn 3'?",
+        answers: [
+            "Haran Flute",
+            "Haran Jazz",
+            "Haran Banjo",
+            "Haran Piano"
+        ],
+        correctAnswer : "Haran Banjo"   
+    },
+    {
+        question : "In the Manga 'Hokuto no Ken', the protagonist is the successor of which famous School? ",
+        answers: [
+            "Nanto",
+            "Goshasei",
+            "Seito",
+            "Hokuto"
+        ],
+        correctAnswer : "Hokuto"
+    },
+
+    {
+        question : "Who’s the Author of the famous Manga 'Galaxy Express 999'?",
+        answers: [
+            "Leiji Matsumoto",
+            "Akira Toriyama",
+            "Yoshihiro Togashi",
+            "Go Nagai"
+        ],
+        correctAnswer : "Leiji Matsumoto"
+    },
+    
+    {
+        question : "In the Manga 'Tokimeki Tonight', which type of monster is the mother of Ranze?" ,
+        answers: [
+            "A vampire",
+            "A ghost",
+            "A werewolf",
+            "A zombie"
+        ],
+        correctAnswer: "A werewolf"
+    },
+    {
+        question : "In the Anime “Yattaman', what is the name of the main Villain female? ",
+        answers: [
+            "Tonzura",
+            "Dokurobei",
+            "Boyacky",
+            "Doronjo"
+        ],
+        correctAnswer : "Doronjo"
+    },
+    {
+        question: "What is the nationality of Roberto Hongo, coach in 'Captain Tsubasa'?",
+        answers: [
+            "Venezuelan",
+            "Portorican",
+            "Spanish", 
+            "Brazilian"
+        ],
+        correctAnswer: "Brazilian"
+    }
+];
---
+++ index.js
@@ -0,0 +1,151 @@
+let questionNumber = 0;
+let score = 0;
+
+//generate question html
+function generateQuestion () {
+  if (questionNumber < STORE.length) {
+    return `<div class="question-${questionNumber}">
+    <h2>${STORE[questionNumber].question}</h2>
+    <form>
+    <fieldset>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[0]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[1]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[2]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[3]}</span>
+    </label>
+    <button type="submit" class="submitButton">Submit</button>
+    </fieldset>
+    </form>
+    </div>`;
+} else {
+    renderResults();
+    restartQuiz();
+    $('.questionNumber').text(10)
+  }
+}
+
+//increment question number
+function changeQuestionNumber () {
+  //if (questionNumber < STORE.length) {
+    questionNumber ++;
+  //}
+  $('.questionNumber').text(questionNumber+1);
+}
+
+//increment score
+function changeScore () {
+  score ++;
+}
+
+//start quiz
+//on startQuizButton click hide start div
+//unhide quiz form div
+function startQuiz () {
+  $('.quizStart').on('click', '.startButton', function (event) {
+    $('.quizStart').remove();
+    $('.questionAnswerForm').css('display', 'block');
+    $('.questionNumber').text(1);
+});
+}
+
+// render question in DOM
+function renderQuestion () {
+  $('.questionAnswerForm').html(generateQuestion());
+}
+
+//user selects answer on submit run user feedback
+function userSelectAnswer () {
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+    let selected = $('input:checked');
+    let answer = selected.val();
+    let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+    if (answer === correctAnswer) {
+      selected.parent().addClass('correct');
+      ifAnswerIsCorrect();
+    } else {
+      selected.parent().addClass('wrong');
+      ifAnswerIsWrong();
+    }
+  });
+}
+
+function ifAnswerIsCorrect () {
+  userAnswerFeedbackCorrect();
+  updateScore();
+  addPinkyBadge();
+}
+
+function ifAnswerIsWrong () {
+  userAnswerFeedbackWrong();
+  addDarkBadge();
+}
+
+//user feedback for correct answer
+function userAnswerFeedbackCorrect () {
+  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+  $('.questionAnswerForm').html(`<div class="correctFeedback"><div class="icon"><img src="correctAnspic.jpg" alt="Kokori is happy of your answer"/></div><p><b>You got it right!</b></p><button type=button class="nextButton">Next</button></div>`);
+}
+
+function addPinkyBadge () {
+    $('.col-4').append('<img src="successicon.png" alt="that is a success icon" class="sakura"/>');
+}
+
+//user feedback for wrong answer
+function userAnswerFeedbackWrong () {
+  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+  // let iconImage = `${STORE[questionNumber].icon}`;
+  $('.questionAnswerForm').html(`<div class="correctFeedback"><div class="icon"><img src="failedQuestion.jpg" alt="Nike and Kokori are scared of your answer"/></div><p><b>You got it wrong</b><br>the correct answer is <span>"${correctAnswer}"</span></p><button type=button class="nextButton">Next</button></div>`);
+}
+
+function addDarkBadge () {
+    $('.col-4').append('<img src="darkCircle.png" alt="that is an unsuccess icon" class="dark"/>');
+}
+
+//update score text
+function updateScore () {
+  changeScore();
+  $('.score').text(score);
+}
+
+//when quiz is over this is the html for the page
+function renderResults () { 
+    $('.questionAnswerForm').html(`<div class="results correctFeedback"><h3>You made it through the end!</h3><img src="endingimage.jpg" alt="Nike and Kokoki to the next adventure"/><p>You got ${score} / 10</p><p>Success or failure, is not easy to be a nerd.</p><button class="restartButton">Start again :)</button></div>`);
+}
+
+//what happens when the user clicks next
+function renderNextQuestion () {
+  $('main').on('click', '.nextButton', function (event) {
+    changeQuestionNumber();
+    renderQuestion();
+    userSelectAnswer();
+  });
+}
+
+//restart quiz function - reloads page to start quiz over
+function restartQuiz () {
+  $('main').on('click', '.restartButton', function (event) {
+    location.reload();
+  });
+}
+
+//run quiz functions
+function createQuiz () {
+  startQuiz();
+  renderQuestion();
+  userSelectAnswer();
+  renderNextQuestion();
+}
+
+$(createQuiz);
